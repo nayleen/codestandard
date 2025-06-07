a933be85391df7c4a166b6ebb6ab2b05ed33220b
Nayleen <almightynay@gmail.com>
2025-06-07 03:32:54 +0200

    spring cleaning; drop nayleen/development-dependencies metapackage

--- tests/CodeStandardTest.php
+++ tests/CodeStandardTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Nayleen;
 
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,33 +22,25 @@ final class CodeStandardTest extends TestCase
         $this->codeStandard = new CodeStandard(projectDirectory: $projectDirectory);
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function allows_risky_fixers(): void
     {
         self::assertTrue($this->codeStandard->getRiskyAllowed());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function config_is_named_correctly(): void
     {
         self::assertSame('Nayleen', $this->codeStandard->getName());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function indents_with_four_spaces(): void
     {
         self::assertSame('    ', $this->codeStandard->getIndent());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function rules_are_based_on_psr_twelve(): void
     {
         self::assertArrayHasKey('@PSR12', $this->codeStandard->getRules());
